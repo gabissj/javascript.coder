b08139339da3e8b2963b96f28ac0fb1ebb92836f
Gabriel Gonzales <gabrielgonzales.18@outlook.es>
2023-09-19 15:39:01 -0300

    Proyecto Final

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ Swal.fire({
 // fetch
 let listado = document.getElementById("listado");
 
-fetch("./data.json")
+fetch("./data.json ")
 .then((response) => response.json())
 .then((data) => {
     data.forEach((item) => {
